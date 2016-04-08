00b1bc59a9d7004f305e322d349ec5761098068a
Aravind V <zerodividedby0@gmail.com>
2016-04-09 00:53:43 +0530

    added an installer for dependencies

---
+++ install.sh
@@ -0,0 +1,16 @@
+echo "Hello! Thank you for you interest in Python Hangman game."
+echo "We will now install the game dependencies for you,"
+echo "Make sure, you are connected to the internet, and type in your passwords, when"
+echo "installer asks you to."
+sudo apt-get update
+sudo apt-get install python2.7
+sudo apt-get install python-Tk
+sudo apt-get install python-imaging-tk
+clear
+echo "great! Now that we have all the dependencies installed,"
+echo "you can now play the game"
+echo "To start game, fire up a console, and type"
+echo "\$ python hangman.py"
+echo "If you have any issues with installation or game play, you can reachout to us"
+echo "at email: zerodividedby0@gmail.com"
+echo "Thank-you!"
