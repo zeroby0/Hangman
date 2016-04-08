cac5af32ef056c137dc227c4c4ae82eb2f1d861f
Aravind V <zerodividedby0@gmail.com>
2016-04-09 00:30:46 +0530

    Added game dependencies

---
+++ HangMan_img/words.py
@@ -0,0 +1,161 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+science_beginner = [
+    'science',
+    'planet',
+    'space',
+    'fog',
+    'weather',
+    'sulphur',
+    'water',
+    'processor',
+    'amplify',
+    'pyramid',
+    'sphere',
+    'circle',
+    'square',
+    'light',
+    'force',
+    'vacuum',
+    'opaque',
+    ]
+science_intermediate = [
+    'satellite',
+    'orbit',
+    'ellipse',
+    'chemistry',
+    'physics',
+    'biology',
+    'magnetism',
+    'genetics',
+    'climate',
+    'algorithm',
+    'alchemist',
+    'cancer',
+    'shadow',
+    'eclipse',
+    ]
+science_master = [
+    'trajectory',
+    'parabola',
+    'cristal',
+    'stellar',
+    'tripod',
+    'labster',
+    'histor'
+    ]
+
+science = {1: science_beginner, 2: science_intermediate,
+           3: science_master}
+
+animals_beginner = [
+    'ant',
+    'rat',
+    'dog',
+    'cat',
+    'wolf',
+    'zebra',
+    'snake',
+    'bear',
+    'pig',
+    'hamster',
+    'eel',
+    'eagle',
+    'vulture',
+    'dodo',
+    'flamingo',
+    'firefly',
+    ]
+animals_master = [
+    'hippopotamus',
+    'elephant',
+    'giraffe',
+    'heyna',
+    'rhinoceros',
+    'monkey',
+    'penguin',
+    'rabbit',
+    'kangaroo',
+    'octopus',
+    'scorpion',
+    'squirrel',
+    'butterfly',
+    'caterpillar',
+    'centipede',
+    'cephalpod',
+    'chihuahua',
+    'chimpanzee',
+    ]
+animals_intermediate = [
+    'vixen',
+    'cow',
+    'donkey',
+    'lion',
+    'tiger',
+    'panda',
+    'horse',
+    'sheep',
+    'deer',
+    'fish',
+    'duck',
+    'oyster',
+    'python',
+    'gorilla',
+    'monkey',
+    'bluebird',
+    'chipmunk',
+    'camel',
+    'dragonfly',
+    'beatle',
+    ]
+animals = {1: animals_beginner, 2: animals_intermediate,
+           3: animals_master}
+
+fruits_beginner = [
+    'kiwi',
+    'lime',
+    'plum',
+    'apple',
+    'lemon',
+    'date',
+    'grape',
+    'pear',
+    'peach',
+    'papaya',
+    'orange',
+    'mango',
+    ]
+fruits_intermediate = [
+    'pumpkin',
+    'tomato',
+    'guava',
+    'coconut',
+    'banana',
+    'apricot',
+    'sapota',
+    'cashew',
+    'noni',
+    'lemon',
+    'cherries',
+    'broccoli',
+    ]
+fruits_master = [
+    'honeydew',
+    'persimmon',
+    'jackfruit',
+    'lychee',
+    'watermelon',
+    'cherymoya',
+    'rambutan',
+    'strawberry',
+    'durian',
+    'pomelo',
+    ]
+
+fruits = {1: fruits_beginner, 2: fruits_intermediate, 3: fruits_master}
+
+worddatabase = {1: science, 2: animals, 3: fruits}
+
+
+			
